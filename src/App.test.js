ef1bf94238d58cc2d56c0ab5e8817984f9f53e8e
Mayank Kamboj <mayankkamboj47@gmail.com>
2021-06-28 11:13:33 +0530

    Setup project
    1. Arranged directory
    2. Added some commentary
    3. Installled packages

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,11 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+// Expect the opening screen to show : 
+// A textbox with a default passage written inside of it
+// A button
+// A title that says 'Enter text to speedread'
+
+
+// Expect the screen to show a word and 3 buttons when the button is clicked.
+// Expect a second component to appear now.
